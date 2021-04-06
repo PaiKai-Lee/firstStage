def89b1e196c200409605d57fc02a221f9343f96
unknown <b790718@gmail.com>
2021-04-06 16:55:31 +0800

    first

---
+++ main.py
@@ -0,0 +1,95 @@
+from flask import Flask,request,render_template,redirect,session,url_for
+import mysql.connector
+
+app=Flask(__name__)
+app.secret_key=b'8$"\xf9.o\xa0e0Yg\xd7\xdd\xdb\xfc\xaf'
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="abcd1234",
+    database="website"
+)
+mycursor=mydb.cursor()
+apiUsers={}
+@app.route("/")
+def index():
+    if "username" in session:
+        return redirect(url_for("member"))
+    else:
+        return render_template("index.html")
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    user=request.form["user"]
+    password=request.form["password"]
+    
+    mycursor.execute("SELECT username,password,name FROM user WHERE username=%s and password=%s",(user,password)) #在資料庫中選擇與輸入相同的帳密
+    DBinf=mycursor.fetchone()
+    if DBinf != None:      #判斷帳號密碼是否存在資料庫
+            session["username"]=user
+            session["name"]=DBinf[2]
+            session.permanent= True
+            return redirect(url_for("member"))
+    return redirect(url_for("error",Message="帳號或密碼輸入錯誤"))
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    name=request.form["name"]
+    user=request.form["user"]
+    password=request.form["password"]
+    valid=request.form["validation"]
+
+    if name=="" or user=="" or password=="":    #註冊資料不可空白
+        return redirect(url_for("error",Message="資料不可空白"))
+    elif valid != password:                     #密碼驗證
+        return redirect(url_for("error",Message="密碼驗證錯誤"))
+    mycursor.execute("SELECT username FROM user where username = %s",(user,))#在資料庫中選擇與輸入相同的username
+    DBuser=mycursor.fetchone()     
+    if DBuser != None:   #判斷帳號是否重複(是否存在資料庫)
+        return redirect(url_for("error",Message="帳號已被註冊"))
+
+    sql=("INSERT INTO user(name,username,password) VALUES (%s,%s,%s)") #輸入SQL語法
+    val=(name,user,password)
+    mycursor.execute(sql,val)   #執行SQL語法
+    mydb.commit()               #更改資料記得要commit
+    return redirect(url_for("index"))
+
+@app.route("/signout")
+def signout():
+    session.pop("username",None)
+    session.pop("name",None)
+    return redirect(url_for("index"))
+
+@app.route("/member/")
+def member():
+    user=session["name"]
+    return render_template("member.html",user=user)
+
+@app.route("/error/")
+def error():
+    if "username" in session:
+        return redirect(url_for("member"))
+    Message=request.args.get("Message")
+    return render_template("error.html",errorMessage=Message)
+
+#user資料API
+@app.route("/api/users")
+def api():
+    val=request.args.get("username")
+    mycursor.execute("SELECT id,name,username From user where username=%s",(val,))
+    DBuser=mycursor.fetchone()
+    if DBuser==None:
+        apiUsers["data"]=DBuser
+        return apiUsers
+    userID=DBuser[0]
+    name=DBuser[1]
+    userName=DBuser[2]
+    apiUsers["data"]={"id":userID,"name":name,"username":userName}
+    return apiUsers
+    
+        
+    
+
+if __name__=="__main__":
+    app.run(port=3000)
---
+++ member.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'test1','test1','121212','2021-03-29 15:39:26'),(2,'test2','test2','test2','2021-03-29 15:39:51'),(3,'Pai-Kai Lee','test','test','2021-03-29 21:24:09'),(4,'卍煞氣ㄟ卍','qwer1234','qwer1234','2021-03-30 21:11:35'),(6,'ply','ply','ply','2021-03-31 10:29:57'),(7,'king','king','king','2021-03-31 10:36:32'),(12,'www','user','password','2021-03-31 12:34:06'),(15,'www','usddddser','password','2021-03-31 12:35:14'),(16,'www','u','password','2021-03-31 12:35:29'),(18,'123','123','123','2021-03-31 12:57:33');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-31 13:42:16
